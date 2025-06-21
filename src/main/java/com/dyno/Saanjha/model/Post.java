0317773b3c6a44ad91d1fffc43df5a363073de60
sumitshresht <shreshtsumit@gmail.com>
2025-06-21 19:53:19 +0530

    fix issue post content limit

--- src/main/java/com/dyno/Saanjha/model/Post.java
+++ src/main/java/com/dyno/Saanjha/model/Post.java
@@ -22,6 +22,7 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long postId;
+    @Column(columnDefinition = "TEXT")
     private String postContent;
     private String imageUrl;
     @CreationTimestamp
